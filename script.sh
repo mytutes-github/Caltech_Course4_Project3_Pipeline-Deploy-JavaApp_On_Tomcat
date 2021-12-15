34a2197829cb266ca475e4ff9a9412264dee48d8
mytutes-github <prahuram.tutes@gmail.com>
2021-12-16 00:37:06 +0530

    Updated the comments in Scritps.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
-# Tomcat Dependencies #
+## Tomcat Dependencies ##
 mv /usr/local/tomcat/webapps /usr/local/tomcat/webapps2
 mv /usr/local/tomcat/webapps.dist/ /usr/local/tomcat/webapps
 mv /usr/local/tomcat/webapps2/* /usr/local/tomcat/webapps
